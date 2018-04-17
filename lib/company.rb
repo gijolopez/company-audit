bcc4470a5015b5df0c2014a35cb6cb0b92bddbeb
gijolopez <glopez22@me.com>
2018-04-17 11:05:28 -0600

    added employee load file and tests

--- lib/company.rb
+++ lib/company.rb
@@ -3,11 +3,33 @@ require './lib/employee'
 require './lib/project'
 
 class Company
-  attr_reader :employees, :projects, :timesheets
+  attr_reader :employees,
+              :projects,
+              :timesheets
 
   def initialize
     @employees = []
     @projects = []
     @timesheets = []
+  end
+
+  def load_employees(filepath)
+    result = check_employee_row_length(filepath)
+    return { success: false, error: 'bad data' } if result.include?(false)
+    result = { success: true, error: nil }
+    employee_csv_parse(filepath)
+    result
+  end
+
+  def check_employee_row_length(filepath)
+    rows = []
+    CSV.foreach(filepath) {|row| rows << row.length}
+    rows.map {|num| num == 5}
+  end
+
+  def employee_csv_parse(filepath)
+    CSV.foreach(filepath) do |row|
+      @employees << Employee.new(row[0], row[1], row[2], row[3], row[4])
+    end
   end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -14,4 +14,17 @@ class CompanyTest < Minitest::Test
     assert_equal [], company.projects
     assert_equal [], company.timesheets
   end
+
+  def test_it_can_load_employees
+    company = Company.new
+    result = company.load_employees('./data/employees.csv')
+    assert_instance_of Hash, result
+  end
+
+  def test_it_does_not_load_bad_employee_file
+    company = Company.new
+    result = company.load_employees('./data/bad_employees.csv')
+    assert_instance_of Hash, result
+  end
+
 end
